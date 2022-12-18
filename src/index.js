97bb77904f1feccd885b99b7ee7a61eb3ec1e75e
sojin.son <sojin.son96@gmail.com>
2022-12-18 13:17:08 +0900

    18.2 미들웨어란?

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware} from 'redux';
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import rootReducer from './modules';
+import loggerMiddleware from './lib/loggerMiddleware';
+import { createLogger } from 'redux-logger';
 
-
-const store = createStore(rootReducer);
+const logger = createLogger();
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
